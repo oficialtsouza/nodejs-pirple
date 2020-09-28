62f590290cbbd59196bb32f02abb3e521194dfce
oficialtsouza <oficialtsouza@gmail.com>
2020-09-28 19:00:01 -0400

    Parse the url and remove '/' from the begining and end of path

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // Dependencies
 const http = require("http");
+const url = require("url");
 
-//Create server
+// Create server
 var server = http.createServer((req, res) => {
+    // Get the url and parse it
+    // true  value to parse the query string
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+    // Send the response
     res.end("Hello World");
+
+    // Log the request path
+    console.log(`Request revieved on path:  ${trimmedPath}`);
 });
 
+// Start server on port 3000
 server.listen(3000, () => {
     console.log("Server running on port 3000");
 });
