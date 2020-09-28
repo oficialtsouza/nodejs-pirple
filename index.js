1efb0b90b92a03dd8312c66d702668574e3116ea
oficialtsouza <oficialtsouza@gmail.com>
2020-09-28 19:09:53 -0400

    Parsing url method and query string

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const url = require("url");
 
 // Create server
 var server = http.createServer((req, res) => {
-    // Get the url and parse it
-    // true  value to parse the query string
+    // Get the url and parse it, true  value to parse the query string
     var parsedUrl = url.parse(req.url, true);
 
     // Get the path
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
+    // Get the HTTP Method in lowercase
+    var method = req.method.toLowerCase();
+
+    // Get the query string as object
+    var queryObject = parsedUrl.query;
+
     // Send the response
     res.end("Hello World");
 
     // Log the request path
-    console.log(`Request revieved on path:  ${trimmedPath}`);
+    console.log(
+        `Request revieved on path ${trimmedPath} , with method ${method}`
+    );
+    console.log("Query object " + toString(queryObject));
 });
 
 // Start server on port 3000
