67a791f83d575a18fbe6e2ece83e3909786efa69
oficialtsouza <oficialtsouza@gmail.com>
2020-11-10 21:46:04 -0400

    add checks routes

--- config.js
+++ config.js
@@ -4,14 +4,16 @@ environments.staging = {
     httpPort: 3000,
     httpsPort: 3001,
     envName: "staging",
-    hashingSecret: "H4S1NGS3C3T"
+    hashingSecret: "H4S1NGS3C3T",
+    maxChecks: 5
 };
 
 environments.production = {
     httpPort: 5000,
     httpsPort: 5001,
     envName: "production",
-    hashingSecret: "H4S1NGS3C3T"
+    hashingSecret: "H4S1NGS3C3T",
+    maxChecks: 5
 };
 
 const currentEnvironment = process.env.NODE_ENV ?
